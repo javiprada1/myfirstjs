1c1135c9a99a22611826a27b8b2be3809923316c
javiprada1 <javiprada1@gmail.com>
2022-02-01 14:56:03 +0100

    Add Magic button and files.

---
+++ js/magic.js
@@ -0,0 +1,10 @@
+function myFirstFunction(){
+    console.log("Thank you for you click;");
+    var div = document.getElementById("my_div");
+    div.classList.add("my_class");
+    console.log(div);
+}
+
+function mySecondFunction(){
+    console.log("Thank you for you interest;");
+}
